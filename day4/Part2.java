e31c3fcf8e205f553c1590dc407cfbe6b768db10
Virendra115 <mvirendra115@google.com>
2022-12-05 12:24:18 +0530

    day 4

---
+++ day4/Part2.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Part2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int ans = 0;
+        while (sc.hasNextLine()) {
+            String[] arr = sc.nextLine().split(",");
+            int[] a = Arrays.stream(arr)
+                    .flatMap(str -> Arrays.stream(str.split("-")))
+                    .map(Integer::parseInt)
+                    .mapToInt(i -> i)
+                    .toArray();
+            if (a[0] >= a[2] && a[0] <= a[3]) {
+                ans++;
+            } else if (a[1] >= a[2] && a[1] <= a[3]) {
+                ans++;
+            } else if (a[2] >= a[0] && a[2] <= a[1]) {
+                ans++;
+            } else if (a[2] >= a[0] && a[3] <= a[1]) {
+                ans++;
+            }
+        }
+        System.out.println(ans);
+    }
+}
